04e6a793e3b86f9db42eed2943ff5eef96d01be3
hegaz0y <iphonemen00@gmail.com>
2022-06-16 02:24:53 +0200

    Anoubis

---
+++ Anoubis.py
@@ -0,0 +1,36 @@
+import os
+import time
+import subprocess
+
+from ppadb.client import Client as AdbClient
+
+from Anoubis.utils import logger
+from Anoubis.utils import config
+from Anoubis.utils import mtksu
+
+def main():
+    os.system(config.CLEAN)
+    try:
+        subprocess.call(['adb', 'start-server'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT)
+    except Exception as e:
+        logger.die(f"Could not start the ADB server ({e})!", 3)
+
+    client = AdbClient(host=config.DEFAULT_IP, port=5037)
+    print(config.BANNER, config.VERSION, config.MENU_OPTIONS)
+
+    opt = 0
+    while opt == 0 or opt > 4:
+        try:
+            opt = int(input("[I]: Please select >> "))
+        except ValueError:
+            pass
+
+    if opt == 4:
+        exit("")
+
+    mtksu.cmd_handler(client, opt)
+
+if __name__ == '__main__':
+    main()
